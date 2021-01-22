7f4c33f832e771508a34cfdb7ff8f9508d6a0c31
Gabriela Šejnová <sejnogab@ciirc.cvut.cz>
2021-01-22 19:35:04 +0100

    deleted unnecessary root files

--- myGym/download_textures.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget -r --no-parent --reject "index.html*" https://data.ciirc.cvut.cz/public/groups/incognite/myGym/dtdseamless/warpedImages/
-
-mv -T data.ciirc.cvut.cz/public/groups/incognite/myGym/dtdseamless/warpedImages ./envs/dtdseamless
-
-rm -r data.ciirc.cvut.cz
